eaec5e4259305298f282b563ef0b248792cf4570
Alexandr <vorblex@gmail.com>
2018-09-12 17:42:57 +0300

    First commit

---
+++ gulp/paths/js.foundation.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = [
+  './node_modules/jquery/dist/jquery.min.js',
+  './node_modules/svg4everybody/dist/svg4everybody.min.js',
+  './node_modules/tapjs/dist/tap.min.js',
+  './node_modules/conic-gradient/conic-gradient.js'
+];
